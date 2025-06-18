789595ce65cc35bc78e60760205b826c8fe33859
Patrik Svensson <patrik.svensson@manyone.com>
2025-06-18 10:10:17 +0200

    fix: Delaying set up to ensure layout is ready

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-import './style.css'
-import './skew-effect.css'
-import { initSkewEffect } from './skew-effect';
+import "./style.css";
+import "./skew-effect.css";
+import { initSkewEffect } from "./skew-effect";
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.ts'
 
-
-// document.addEventListener("DOMContentLoaded", () => {
-window.onload = () => {
-  console.log("Dom ready, setting up skew effect");
-  const app = document.getElementById("app")!;
-  initSkewEffect(app);
-};
-// });
+// HAck to ensure layout is ready for measuring placement of skew elements
+// Was experienceing the elements to be placed at the top, since the measurement happened before centering was applied by css.
+window.addEventListener("load", () => {
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      console.log("laoded, deffered two animation frames -> setting up skew effect");
+      const app = document.getElementById("app")!;
+      initSkewEffect(app);
+    });
+  });
+});
